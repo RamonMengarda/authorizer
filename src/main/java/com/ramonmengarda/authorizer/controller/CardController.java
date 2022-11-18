87cd9a495cf55e3a43a09432b4cdb04ac38989fd
Ramon Mengarda <ramon_hiago@hotmail.com>
2022-11-18 03:35:11 -0300

    feat: include rest method for checking balance

--- src/main/java/com/ramonmengarda/authorizer/controller/CardController.java
+++ src/main/java/com/ramonmengarda/authorizer/controller/CardController.java
@@ -1,5 +1,8 @@
 package com.ramonmengarda.authorizer.controller;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -7,10 +10,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ramonmengarda.authorizer.exceptions.NonexistentCardException;
 import com.ramonmengarda.authorizer.model.Card;
 import com.ramonmengarda.authorizer.service.CardService;
 
@@ -45,5 +51,24 @@ public class CardController {
                     "Card creation is locked due to too many simultaneous requests for the database. Try again.");
             return ResponseEntity.status(HttpStatus.LOCKED).body(responseBody);
         }
+    }
+
+    @GetMapping("/cartoes/{number}")
+    public ResponseEntity<BigDecimal> getCardBalance(@PathVariable Long number) {
+
+        try {
+            List<Card> cardList = cardService.getCardsByNumber(number);
+
+            if (cardList.isEmpty()) {
+                throw new NonexistentCardException("CARTAO_INEXISTENTE");
+            }
+
+            Card card = cardList.get(0);
+
+            return ResponseEntity.status(HttpStatus.OK).body(card.getBalance());
+        } catch (NonexistentCardException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
     }
 }
