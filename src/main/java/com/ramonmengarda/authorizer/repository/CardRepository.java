042f224877645a36653d684f2ccbd2cb275fd1d8
Ramon Mengarda <ramon_hiago@hotmail.com>
2022-11-18 03:17:36 -0300

    fix: import java util list

--- src/main/java/com/ramonmengarda/authorizer/repository/CardRepository.java
+++ src/main/java/com/ramonmengarda/authorizer/repository/CardRepository.java
@@ -1,5 +1,7 @@
 package com.ramonmengarda.authorizer.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.ramonmengarda.authorizer.model.Card;
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
 
-    List<Card> findByNumber(Long number);
+    Listt<Card> findByNumber(Long number);
 }
